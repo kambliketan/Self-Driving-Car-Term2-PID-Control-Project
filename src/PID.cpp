742644a68cabaaa8e9a2875dd6cb44da38abb1b4
Ketan Kambli <kambliketan@gmail.com>
2018-02-16 08:43:44 -0800

    initial changes

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <math.h>
 
 using namespace std;
 
@@ -10,12 +11,26 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp, double Ki, double Kd)
+{
+    Kp_ = Kp;
+    Ki_ = Ki;
+    Kd_ = Kd;
+
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte)
+{
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error = i_error + cte;
 }
 
-double PID::TotalError() {
+double PID::TotalError()
+{
+    return - 1.0 * (Kp_ *  p_error + Ki_ * i_error + Kd_ * d_error);
 }
 
